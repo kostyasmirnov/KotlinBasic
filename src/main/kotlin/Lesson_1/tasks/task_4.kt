36dea633a7235c19135ee3f577db6b35230e0aec
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-06 12:26:51 +0300

    KS-1-4 output information about first space flight

---
+++ src/main/kotlin/Lesson_1/tasks/task_4.kt
@@ -0,0 +1,19 @@
+package Lesson_1.tasks
+
+fun main() {
+
+    val length: Long = 40868600000L
+    val age: Int = 27
+    val partOfDay: Double = 0.075
+    val seconds: Int = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+
+}
