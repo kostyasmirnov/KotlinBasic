ca1b11510b5a01f2848474a92d5d1317c25edf83
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-07 20:13:29 +0300

    fix

---
+++ src/main/kotlin/Lesson_1/tasks/task_2.kt
@@ -0,0 +1,16 @@
+package Lesson_1.tasks
+
+fun main() {
+
+    var amountOfOrders: Int = 75
+    val thanksForPurchase: String = "Благодаронсть за покупку"
+    println("Количество заказов: " + amountOfOrders)
+    println(thanksForPurchase)
+
+    var employees: Int = 2000
+    //println("Количество работников интернет магазина: " + employees)
+
+    employees = employees - 1
+    println("Количество работников интернет магазина: " + employees)
+
+}
