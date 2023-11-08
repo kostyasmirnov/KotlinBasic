7370237e53571f735f91f2be461b7a422b7b9244
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-08 21:24:45 +0300

    KS-2-3 time arrival

---
+++ src/main/kotlin/Lesson_2/task_3.kt
@@ -0,0 +1,18 @@
+package Lesson_2
+
+fun main() {
+
+    val timeDepartureInHours: Int = 9
+    val timeDepartureInMinutes: Int = 39
+
+    val timeOnTheWay: Int = 457
+
+    val travelHours = timeOnTheWay / 60 //7.616
+    val travelMinutes = timeOnTheWay % 60 // 0.65
+
+    val arrivalHour = (timeDepartureInHours + travelHours + (timeDepartureInMinutes + travelMinutes) / 60) % 24
+    val arrivalMinute = (timeDepartureInMinutes + travelMinutes) % 60
+
+    println("Время прибытия поезда: $arrivalHour:$arrivalMinute")
+
+}
