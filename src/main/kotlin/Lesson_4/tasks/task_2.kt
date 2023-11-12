9dfce21957eabd899062e4c2967de006dd6b2d64
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-12 13:42:15 +0300

    KS-4-2 fix

--- src/main/kotlin/Lesson_4/tasks/task_2.kt
+++ src/main/kotlin/Lesson_4/tasks/task_2.kt
@@ -2,18 +2,18 @@ package Lesson_4.tasks
 
 fun main() {
 
-    val fishKg = 20
-    val milkLiter = 80
-    val goldKg = 50
-    val waterLiter = 100
-
-    val maxLiters = 100
-    val maxWeight = 100
-    val minWeight = 35
-
     println("Груз с весом $fishKg кг и объемом $milkLiter л соответствует категории 'Average': " +
     "${(minWeight > fishKg && fishKg < maxWeight) && (milkLiter < maxLiters)}")
     println("Груз с весом $goldKg кг и объемом $waterLiter л соответствует категории 'Average': " +
-            "${(minWeight > goldKg && goldKg < maxWeight) && (waterLiter < maxLiters)}")
+            "${(minWeight >= goldKg && goldKg <= maxWeight) && (waterLiter < maxLiters)}")
 
 }
+
+const val fishKg = 20
+const val milkLiter = 80
+const val goldKg = 50
+const val waterLiter = 100
+
+const val maxLiters = 100
+const val maxWeight = 100
+const val minWeight = 35
