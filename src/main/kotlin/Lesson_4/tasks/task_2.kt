34c560d606b12058fbc771876266eb1918cef3f5
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-13 16:21:42 +0300

    KS-4-2 fix name const, output info, variables

--- src/main/kotlin/Lesson_4/tasks/task_2.kt
+++ src/main/kotlin/Lesson_4/tasks/task_2.kt
@@ -2,18 +2,18 @@ package Lesson_4.tasks
 
 fun main() {
 
+    val fishKg = 20
+    val milkLiter = 80
+    val goldKg = 50
+    val waterLiter = 100
+
     println("Груз с весом $fishKg кг и объемом $milkLiter л соответствует категории 'Average': " +
-    "${(minWeight > fishKg && fishKg < maxWeight) && (milkLiter < maxLiters)}")
+    "${(MIN_WEIGHT >= fishKg && fishKg <= MAX_WEIGHT) && (milkLiter < MAX_LITERS)}")
     println("Груз с весом $goldKg кг и объемом $waterLiter л соответствует категории 'Average': " +
-            "${(minWeight >= goldKg && goldKg <= maxWeight) && (waterLiter < maxLiters)}")
+            "${(MIN_WEIGHT >= goldKg && goldKg <= MAX_WEIGHT) && (waterLiter < MAX_LITERS)}")
 
 }
 
-const val fishKg = 20
-const val milkLiter = 80
-const val goldKg = 50
-const val waterLiter = 100
-
-const val maxLiters = 100
-const val maxWeight = 100
-const val minWeight = 35
+const val MAX_LITERS = 100
+const val MAX_WEIGHT = 100
+const val MIN_WEIGHT = 35
