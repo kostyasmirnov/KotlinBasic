89b23a3a04fbc5d176709ec81e0bea60501ddc6e
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-13 17:41:56 +0300

    KS-5-2 check age users

---
+++ src/main/kotlin/Lesson_5/tasks/task_2.kt
@@ -0,0 +1,16 @@
+package Lesson_5.tasks
+
+
+fun main() {
+
+
+    val userYearOfBirth = readLine()!!.toInt()
+
+    val result = if (YEAR_NOW - userYearOfBirth >= MIN_AGE_USER ) "Показать экран со скрытым контентом"
+    else "Доступ запрещен"
+    println(result)
+
+}
+
+const val YEAR_NOW = 2023
+const val MIN_AGE_USER = 18
