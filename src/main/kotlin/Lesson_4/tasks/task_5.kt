59a2e7ff76d681a98d1864de484b76bf6af56124
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-14 17:26:25 +0300

    KS-4-5 fix

--- src/main/kotlin/Lesson_4/tasks/task_5.kt
+++ src/main/kotlin/Lesson_4/tasks/task_5.kt
@@ -3,12 +3,12 @@ package Lesson_4.tasks
 import com.sun.org.apache.xpath.internal.operations.Bool
 
 fun main() {
-    var isShipWithDamage: Boolean = readlnOrNull()?.toBoolean() ?: false
+    var hasNoDamages: Boolean = readlnOrNull()?.toBoolean() ?: false
     var crew: Int = readlnOrNull()?.toInt() ?: 0
     var isNumberOfBoxesOfProvisions: Int = readlnOrNull()?.toInt() ?: 0
     var isFavorableWeather: Boolean = readlnOrNull()?.toBoolean() ?: false
 
-    if (checkConditions(isShipWithDamage, crew, isNumberOfBoxesOfProvisions, isFavorableWeather) || checkConditions2(isShipWithDamage, crew, isNumberOfBoxesOfProvisions, isFavorableWeather)) {
+    if (checkConditions(hasNoDamages, crew, isNumberOfBoxesOfProvisions, isFavorableWeather) || checkConditions2(hasNoDamages, crew, isNumberOfBoxesOfProvisions, isFavorableWeather)) {
         println("Корабль может отправиться в плавание")
     } else {
         println("Корабль не может отправиться в плавание")
@@ -26,7 +26,7 @@ fun checkConditions(
     isFavorableWeather: Boolean): Boolean {
     return ((isShipWithDamage == true)
             and (crew in MIN_RANGE_CREW..MAX_RANGE_CREW )
-            and (isNumberOfBoxesOfProvisions >= MIN_BOXES))
+            and (isNumberOfBoxesOfProvisions > MIN_BOXES))
 
 }
 
@@ -36,6 +36,6 @@ fun checkConditions2(
     isNumberOfBoxesOfProvisions: Int,
     isFavorableWeather: Boolean): Boolean {
     return ((isFavorableWeather == true)
-            and (crew >= MAX_RANGE_CREW)
-            and (isNumberOfBoxesOfProvisions >= MIN_BOXES))
+            and (crew == MAX_RANGE_CREW)
+            and (isNumberOfBoxesOfProvisions > MIN_BOXES))
 }
