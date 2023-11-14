e743491cdc9cca21fa66fea6d0947a27ee431730
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-14 17:23:16 +0300

    KS-4-4 fix

--- src/main/kotlin/Lesson_4/tasks/task_4.kt
+++ src/main/kotlin/Lesson_4/tasks/task_4.kt
@@ -8,7 +8,7 @@ fun main() {
     val legDay: String = "Упражнения для ног:"
     val backDay: String = "Упражнения для спины:"
     val pressDay: String = "Упражнения для пресса:"
-    val isEvenDay: Boolean = true
+    val isEvenDay: Boolean = currentDay % 2 == 0
 
     var resultInfo = """
         $armDay   ${isEvenDay}
