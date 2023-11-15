b005aa18edb1a02a5ebb2a584a91eaff9a6720b7
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-15 17:42:04 +0300

    KS-5-1 fix, add readln(), add expression

--- src/main/kotlin/Lesson_5/tasks/task_1.kt
+++ src/main/kotlin/Lesson_5/tasks/task_1.kt
@@ -5,12 +5,13 @@ fun main() {
     println("Для входа в приложение необходимо доказать что вы человек \n" +
             "Сколько будет 12+12?")
 
-    var userAnswer = readLine()!!.toInt()
+    val userAnswer = readln()!!.toInt()
+    val expression = 12 + 12
+    val answer = expression + expression
 
-    if (userAnswer == ANSWER) {
+    if (userAnswer == answer) {
         println("Добро пожаловать!")
     } else println("Доступ запрещен")
 
 }
 
-const val ANSWER: Int = 24
