458875ad4d2ca4a30c98530af0badbcb3c0754b8
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-15 18:13:40 +0300

    KS-6-1 registration and authorization

---
+++ src/main/kotlin/Lesson_6/tasks/task_1.kt
@@ -0,0 +1,28 @@
+package Lesson_6.tasks
+
+fun main() {
+
+    println(WELCOME_MESSAGE_LOGIN)
+    val login = readln()
+    println(WELCOME_MESSAGE_PASSWORD)
+    val password: String = readln()
+
+    do {
+        println(INPUT_LOGIN)
+        val enteredLogin = readln()
+        println(INPUT_PASSWORD)
+        val enteredPassword = readln()
+
+        val checkLoginPassword = enteredLogin != login || enteredPassword != password
+        if (checkLoginPassword) println("Такого пользователя не существует. Попробуйте еще раз+\n")
+        else println("Авторизавция прошла успешно")
+
+    } while (checkLoginPassword)
+
+}
+
+const val WELCOME_MESSAGE_LOGIN: String = "Укажите логин:"
+const val WELCOME_MESSAGE_PASSWORD: String = "Укажите пароль:"
+
+const val INPUT_LOGIN: String = "Введите зарегистрированный ранее логин"
+const val INPUT_PASSWORD: String = "Введите зарегистрированный ранее пароль"
