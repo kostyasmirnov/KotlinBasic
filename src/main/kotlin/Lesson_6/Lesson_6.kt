c9a05aeb9f03f5c963be2a43b2cb41e944895b6d
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-15 18:02:10 +0300

    lesson 6 done

---
+++ src/main/kotlin/Lesson_6/Lesson_6.kt
@@ -0,0 +1,19 @@
+package Lesson_6
+
+fun main() {
+
+    // while, do while
+
+    var counter =5
+
+//    while (counter > 0) {
+//        println("Реклама закончится через ${counter--}")
+//        Thread.sleep(1000)
+//    }
+
+    do {
+        println("Реклама закончится через ${counter--}")
+        Thread.sleep(1000)
+    } while (counter > 0)
+
+}
