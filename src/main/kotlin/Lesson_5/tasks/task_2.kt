be876b8e9ec870a6f80811c67e115cdd939ce841
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-16 19:13:17 +0300

    KS-5-2 fix

--- src/main/kotlin/Lesson_5/tasks/task_2.kt
+++ src/main/kotlin/Lesson_5/tasks/task_2.kt
@@ -3,7 +3,7 @@ package Lesson_5.tasks
 
 fun main() {
 
-    val userYearOfBirth = readln()!!.toInt()
+    val userYearOfBirth = readln().toInt()
 
     val result = if (YEAR_NOW - userYearOfBirth >= MIN_AGE_USER ) "Показать экран со скрытым контентом"
     else "Доступ запрещен"
