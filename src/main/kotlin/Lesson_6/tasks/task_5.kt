407997b6e03d5786cb814753beafecd9a4f572c1
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-18 13:21:37 +0300

    KS-6-5 generate mathematical example

---
+++ src/main/kotlin/Lesson_6/tasks/task_5.kt
@@ -0,0 +1,29 @@
+package Lesson_6.tasks
+
+
+
+fun main() {
+
+    println(STARTER_MESSAGE)
+    var counter: Int = 3
+
+    do {
+        var number1 = (1..9).random()
+        var number2 = (1..9).random()
+        println("$number1 + $number2")
+        val userAnswer = readln().toInt()
+        if (userAnswer == number1+number2) println(ACCESS_TRUE)
+        else if (counter > 1) println(TRY_AGAIN)
+        counter--
+    } while (counter > 0)
+
+    println(ACCESS_DENIED)
+
+}
+
+const val STARTER_MESSAGE = "Решите простой математический пример: "
+const val ACCESS_TRUE = "Добро пожаловать!"
+const val TRY_AGAIN = "Попробуйте еще раз!"
+const val ACCESS_DENIED = "Доступ запрещен."
+
+
