1356f1aa7d9b7f29c8fcaada23503211c2106360
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-19 11:34:09 +0300

    KS-6-4 fix

--- src/main/kotlin/Lesson_6/tasks/task_4.kt
+++ src/main/kotlin/Lesson_6/tasks/task_4.kt
@@ -3,19 +3,19 @@ package Lesson_6.tasks
 fun main() {
 
     val randomNumber = (1..9).random()
-
+    var counter = COUNTER
     do {
         val inputNumberFromUser = readln().toInt()
         if (randomNumber == inputNumberFromUser) println(WINNER_MESSAGE)
         else println(INCORRECT_ANSWER_MESSAGE)
-        COUNTER--
-    } while (COUNTER > 0)
+        counter--
+    } while (counter > 0)
 
-    println(LOSER_MESSAGE + randomNumber)
+    println("$LOSER_MESSAGE $randomNumber")
 
 }
 
 const val WINNER_MESSAGE: String = "Это была великолепная игра!"
 const val INCORRECT_ANSWER_MESSAGE: String = "Неверно"
 const val LOSER_MESSAGE: String = "Было загадано число "
-var COUNTER: Int = 5
+const val COUNTER: Int = 5
