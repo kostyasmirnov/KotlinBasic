eb585de52ccf3a4853ab4a06c30df900bc037883
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-19 22:21:55 +0300

    KS-7-1 fix

--- src/main/kotlin/Lesson_7/tasks/task1.kt
+++ src/main/kotlin/Lesson_7/tasks/task1.kt
@@ -3,12 +3,9 @@ package Lesson_7.tasks
 fun main() {
 
     var password: String = ""
-
     for (i in 1..3) {
-        val randomNumbers = (0..9).random()
-        val randomLetters = ('a'..'z').random()
-        password += randomLetters
-        password += randomNumbers
+        password += (0..9).random()
+        password += ('a'..'z').random()
     }
     println(password)
 
