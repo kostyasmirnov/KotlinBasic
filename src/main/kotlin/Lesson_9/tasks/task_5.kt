13cf14e3ebc979d96e7ecd778549813b962e8f87
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-24 02:07:17 +0300

    KS-9-5 create and print list

---
+++ src/main/kotlin/Lesson_9/tasks/task_5.kt
@@ -0,0 +1,22 @@
+package Lesson_9.tasks
+
+import kotlin.math.max
+
+fun main() {
+
+    val ingredients = mutableListOf<String>()
+
+    var count = 1
+    while (count <= 5) {
+        println("Введите ингредиент №$count: ")
+        val ingredient = readln().toString()
+        if (ingredients.contains(ingredient)) println("Такой ингредиент уже добавлен, попробуйте другой")
+        else
+        ingredients.add(ingredient)
+        count++
+    }
+    val ingredientsList = ingredients.sorted()
+    println("Отсортированные ингредиенты: $ingredientsList")
+
+
+}
