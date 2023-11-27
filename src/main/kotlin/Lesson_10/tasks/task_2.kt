bd9e3f842d3c329a761e6689bd3f49e98fe06094
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-27 15:48:05 +0300

    KS-10-2 fix

--- src/main/kotlin/Lesson_10/tasks/task_2.kt
+++ src/main/kotlin/Lesson_10/tasks/task_2.kt
@@ -6,14 +6,20 @@ fun main() {
     val loginUser = readln().toString()
     println(INPUT_PASSWORD)
     val passwordUser = readln().toString()
-    validateCreeds(loginUser, passwordUser)
+    isValidateCreeds(loginUser, passwordUser)
 
+}
+
+fun isValidateCreeds(loginUser: String, passwordUser: String): Boolean {
+    val result = if (loginUser.length < MAX_LENGTH_FOP_CREEDS || passwordUser.length < MAX_LENGTH_FOP_CREEDS) {
+        println(INCORRECT_CREEDS)
+        return false
+    } else return true
+
+    return result
 }
 
 const val INPUT_LOGIN = "Введите логин: "
 const val INPUT_PASSWORD = "Введите пароль: "
 const val INCORRECT_CREEDS = "Логин или пароль недостаточно длинные"
-
-fun validateCreeds(loginUser: String, passwordUser: String) {
-    if (loginUser.length < 4 || passwordUser.length < 4) println(INCORRECT_CREEDS)
-}
+const val MAX_LENGTH_FOP_CREEDS: Int = 4
