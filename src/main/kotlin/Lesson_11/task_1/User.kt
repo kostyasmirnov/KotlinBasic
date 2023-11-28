0073795b4e381b770a0eadb42afcf86bb9707ecb
kasmirnov <kasmirnov@sigma-it.ru>
2023-11-28 21:30:57 +0300

    KS-11-1 fix

--- src/main/kotlin/Lesson_11/task_1/User.kt
+++ src/main/kotlin/Lesson_11/task_1/User.kt
@@ -1,5 +1,27 @@
 package Lesson_11.task_1
 
+fun main() {
+
+    val user1: User = User(
+        id = 1,
+        login = "testik",
+        password = "qwerty123",
+        email = "testikWork@gmail.com",
+    )
+
+    val user2: User = User(
+        id = 2,
+        login = "pixel",
+        password = "stringstring22",
+        email = "google@yahoo.com",
+    )
+
+    user1.printInfoAboutUser
+    println()
+    user2.printInfoAboutUser
+
+}
+
 class User(
     val id: Int,
     val login: String,
