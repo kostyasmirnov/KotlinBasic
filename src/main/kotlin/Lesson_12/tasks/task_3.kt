8745dc8827af7c7db3b8d7a17ae5550ed2a88d65
kasmirnov <kasmirnov@sigma-it.ru>
2023-12-01 20:29:06 +0300

    KS-12-3 init

---
+++ src/main/kotlin/Lesson_12/tasks/task_3.kt
@@ -0,0 +1,25 @@
+package Lesson_12.tasks
+
+fun main() {
+
+    val weather1 = Weather3(30, 20)
+    val weather2 = Weather3(10, 3, true)
+
+    weather1.printWeather3()
+    println()
+    weather2.printWeather3()
+
+}
+
+class Weather3(
+    private var temperatureDayTime: Int,
+    private var temperatureNight: Int,
+    private var wasPrecipitation: Boolean = false,
+) {
+
+    fun printWeather3() {
+        println(temperatureDayTime)
+        println(temperatureNight)
+        println(wasPrecipitation)
+    }
+}
