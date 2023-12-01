8db78ec5e735ea1d07130a971a2fd75fcef35e44
kasmirnov <kasmirnov@sigma-it.ru>
2023-12-01 20:11:00 +0300

    KS-12-2 defining properties in the main constructor

---
+++ src/main/kotlin/Lesson_12/tasks/task_2.kt
@@ -0,0 +1,26 @@
+package Lesson_12.tasks
+
+fun main() {
+
+    val weather1 = Weather2()
+    val weather2 = Weather2(10, 3, true)
+
+    weather1.printWeather()
+    println()
+    weather2.printWeather()
+
+}
+
+class Weather2 (
+    var temperatureDayTime: Int = 20,
+    var temperatureNight: Int = 10,
+    var wasPrecipitation: Boolean = false,
+) {
+
+
+    fun printWeather() {
+        println(temperatureDayTime)
+        println(temperatureNight)
+        println(wasPrecipitation)
+    }
+}
