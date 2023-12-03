1c754fcb8bf6ac35c9bee45ebf73943dfe4b1d3e
kasmirnov <kasmirnov@sigma-it.ru>
2023-12-03 13:25:48 +0300

    KS-11-4 fix. Added ingredients to class "Recipes"

--- src/main/kotlin/Lesson_11/tasks/task_4.kt
+++ src/main/kotlin/Lesson_11/tasks/task_4.kt
@@ -3,60 +3,30 @@ package Lesson_11.tasks
 
 fun main() {
 
-    val CategoryOfBurger = Categories(
-        1, "Бургеры.png", "Рецепты всех популярных видов бургеров",
+    val categoryOfBurger = Category(
+        1, "Рецепты всех популярных видов бургеров",
         "Бургеры", "бургеры.png"
     )
     val classicHamburger = Recipes(
         1, "Классический гамбургер", name = "Бургеры.png", "классический_гамбургер.пнг"
     )
 
-    CategoryOfBurger.getAvatar()
-    CategoryOfBurger.getDescription()
-    CategoryOfBurger.openCategories(CategoryOfBurger.name)
-    println()
-    classicHamburger.getAvatarRecipe()
-    classicHamburger.openRecipe(classicHamburger.name)
-    println()
-    classicHamburger.getIngredients()
-    classicHamburger.countIngredients()
-
 }
 
-class Categories(
+class Category(
     val id: Int = 1,
-    val screenHeader: String = "Категории.png",
     private val description: String = "Рецепты всех популярных видов бургеров",
     val name: String = "Бургеры",
-    private val avatar: String,
-) {
-    fun openCategories(name: String) {
-        println("Переход на экран категорий \"$name\"")
-    }
-
-    fun getDescription(): String {
-        return description
-    }
-
-    fun getAvatar(): String {
-        return avatar
-    }
-}
+    private val preview_image: String,
+)
 
 class Recipes(
     val id: Int = 1,
     val screenHeader: String = "Бургеры",
     val name: String = "",
-    private val avatarRecipe: String = "",
-) : Ingredients(mutableListOf("булка 2", "сыр 4", "помидор 50", "котлета гриль 300"), true, 1) {
-    fun openRecipe(name: String) {
-        println("Открытие экрана с рецептами \"$name\"")
-    }
-
-    fun getAvatarRecipe(): String {
-        return avatarRecipe
-    }
-}
+    private val preview_image: String = "",
+    val ingredients: List<String> = mutableListOf("булка 2", "сыр 4", "помидор 50", "котлета гриль 300")
+)
 
 open class Ingredients(
     private val ingredients: List<String>,
