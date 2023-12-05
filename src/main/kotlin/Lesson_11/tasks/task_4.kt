1c2ecdf3985bf56036fac571e86a2c3d824ade33
kasmirnov <kasmirnov@sigma-it.ru>
2023-12-05 19:02:06 +0300

    KS-11-4 fix

--- src/main/kotlin/Lesson_11/tasks/task_4.kt
+++ src/main/kotlin/Lesson_11/tasks/task_4.kt
@@ -12,7 +12,7 @@ fun main() {
         true, 3, "ФотоБургера.png"
     )
     val classicHamburger = Recipe(
-        1, "Классический гамбургер", "Бургеры", "Бургер.png", ingredients
+        1, "Классический гамбургер", "Бургеры", "Бургер.png", listOf(ingredients)
     )
 
 }
@@ -29,31 +29,14 @@ class Recipe(
     val screenHeader: String = "Бургеры",
     val name: String = "",
     private val previewImage: String = "",
-    val ingredients: Ingredient,
+    val ingredients: List<Ingredient>,
 )
 
 class Ingredient(
-    private val ingredients: List<String> = mutableListOf("булка 2", "сыр 4", "помидор 50", "котлета гриль 300"),
+    val ingredients: List<String> = mutableListOf("булка 2", "сыр 4", "помидор 50", "котлета гриль 300"),
     val isFavorites: Boolean = false,
     private val count: Int = 1,
     val screenHeader: String,
-) {
-
-    fun getIngredients() {
-        ingredients.forEach { println(it) }
-    }
-
-    fun countIngredients() {
-        println(COUNT_SLIDER_MESSAGE)
-        val countSlider = readln().toInt()
-        val updatedList = ingredients.map { ingredients ->
-            val parts = ingredients.split(" ")
-            val name = parts.dropLast(1).joinToString(" ")
-            val quantity = parts.last().toInt() * countSlider
-            "$name $quantity шт"
-        }
-        updatedList.forEach { println(it) }
-    }
-}
+)
 
 const val COUNT_SLIDER_MESSAGE = "Введите количество ингредиентов: "
