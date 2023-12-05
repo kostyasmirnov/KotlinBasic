d8cfacdd04263094b4868ff57f5c28ee3bfb68c3
kasmirnov <kasmirnov@sigma-it.ru>
2023-12-05 19:31:08 +0300

    KS-14-1 class hierarchy design

--- src/main/kotlin/Lesson_14/tasks/task_1.kt
+++ src/main/kotlin/Lesson_14/tasks/task_1.kt
@@ -12,25 +12,27 @@ open class LinerShip(
     val speed: Int = 200,
     val name: String = "Лайнер",
     val numerOfPassengers: Int = 200,
-    //val isCanCrushIce: Boolean = false,
-    //val carrying: Int = 0, //грузоподъемность
 )
 
 class IceBreakerShip(
     name: String = "Ледокол",
     speed: Int = 100,
+    numerOfPassengers: Int = 30,
     val isCanCrushIce: Boolean = true,
     val capacity: Int = 50,
 ) : LinerShip(
     speed = speed,
-    name = name
+    name = name,
+    numerOfPassengers = numerOfPassengers,
 )
 
 class CargoShip(
     name: String = "Грузовой",
     speed: Int = 150,
+    numerOfPassengers: Int = 10,
     val carrying: Int = 150,
 ) : LinerShip(
     speed = 100,
-    name = "Грузовой"
+    name = "Грузовой",
+    numerOfPassengers = numerOfPassengers,
 )
