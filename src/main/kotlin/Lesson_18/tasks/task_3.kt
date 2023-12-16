196f5f0859cff5043e4a51655cdb466959dd4458
kasmirnov <kasmirnov@sigma-it.ru>
2023-12-16 14:29:39 +0300

    KS-18-3 subtyping

---
+++ src/main/kotlin/Lesson_18/tasks/task_3.kt
@@ -0,0 +1,66 @@
+package Lesson_18.tasks
+
+fun main() {
+
+    val fox = Fox("Лиса")
+    val dog = Dog("Собака")
+    val cat = Cat("Кот")
+
+    val tamagotchis = arrayOf(fox, dog, cat)
+
+    tamagotchisActivity(tamagotchis)
+
+}
+
+abstract class Tamagotchi(
+    val name: String,
+) {
+
+    open fun play() = "$name - играет"
+
+    open fun sleep() = "$name - спит"
+
+    abstract fun eat(): String
+
+}
+
+class Fox(
+    name: String,
+) : Tamagotchi(name) {
+
+    override fun play(): String {
+        return super.play()
+    }
+
+    override fun eat(): String = "$name - ест ягоды"
+
+}
+
+class Dog(
+    name: String,
+) : Tamagotchi(name) {
+
+    override fun sleep(): String {
+        return super.sleep()
+    }
+
+    override fun eat(): String = "$name - ест косточки"
+
+}
+
+class Cat(
+    name: String,
+) : Tamagotchi(name) {
+
+    override fun eat(): String = "$name - ест рыбку"
+
+}
+
+fun tamagotchisActivity(tamagotchis: Array<Tamagotchi>) {
+    tamagotchis.forEach {
+        println(it.play())
+        println(it.sleep())
+        println(it.eat())
+    }
+}
+
