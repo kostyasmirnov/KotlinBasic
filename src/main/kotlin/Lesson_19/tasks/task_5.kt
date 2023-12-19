0c3ef391536e003843645b6de327527e597705ef
kasmirnov <kasmirnov@sigma-it.ru>
2023-12-19 14:40:12 +0300

    KS-19-5 fix. use entries for enum class

--- src/main/kotlin/Lesson_19/tasks/task_5.kt
+++ src/main/kotlin/Lesson_19/tasks/task_5.kt
@@ -1,4 +1,8 @@
+@file:OptIn(ExperimentalStdlibApi::class, ExperimentalStdlibApi::class)
+
 package Lesson_19.tasks
+
+
 
 fun main() {
 
@@ -6,15 +10,15 @@ fun main() {
 
     val humanList: MutableList<Human> = mutableListOf()
 
-        for (i in 1..5) {
-            println(INPUT_N)
-            val inputName = readln()
-            println(INPUT_S)
-            val inputSex = readln()
-            val getSex = getSex(inputSex)
-            val human = Human(inputName, getSex)
-            humanList.add(human)
-        }
+    for (i in 1..5) {
+        println(INPUT_N)
+        val inputName = readln()
+        println(INPUT_S)
+        val inputSex = readln()
+        val getSex = getSex(inputSex)
+        val human = Human(inputName, getSex)
+        humanList.add(human)
+    }
 
     humanList.forEach {
         println(it.name)
@@ -25,8 +29,7 @@ fun main() {
 class Human(
     val name: String,
     val sex: Sex,
-) {
-}
+)
 
 enum class Sex(val description: String) {
     MAN("мужчина"),
@@ -35,14 +38,10 @@ enum class Sex(val description: String) {
 }
 
 fun getSex(description: String): Sex {
-    return when (description) {
-        Sex.MAN.description -> Sex.MAN
-        Sex.WOMEN.description -> Sex.WOMEN
-        else -> Sex.OTHER
-    }
+    return Sex.entries.find { it.description == description } ?: Sex.OTHER
 }
 
-const val INSTRUCTIONS_MESSAGE = """Введите имя и пол человек.
+const val INSTRUCTIONS_MESSAGE = """Введите имя и пол человека.
     |Вводите пол в следующем формате: "мужчина" - если это мужчина, и "женщина" - если это женщина.
     |После ввода 5 человек, вам будет выдан список который вы внесли.
 """
