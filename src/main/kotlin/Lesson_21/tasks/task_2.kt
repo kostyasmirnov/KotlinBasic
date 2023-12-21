5907735520f84ec4a91fc6d662e0742c419bacd8
kasmirnov <kasmirnov@sigma-it.ru>
2023-12-21 10:20:34 +0300

    KS-21-2 fix. removed nullability from fun

--- src/main/kotlin/Lesson_21/tasks/task_2.kt
+++ src/main/kotlin/Lesson_21/tasks/task_2.kt
@@ -3,10 +3,10 @@ package Lesson_21.tasks
 fun main() {
 
     val listNumbers: List<Int> = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    println(listNumbers.evenNumbersSum(listNumbers))
+    println(listNumbers.evenNumbersSum())
 }
 
-fun List<Int>.evenNumbersSum(numbers: List<Int>) : Int? {
+fun List<Int>.evenNumbersSum() : Int {
 
     var sum: Int = 0
 
